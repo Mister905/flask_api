8565e87ec9695b8ce77df5b822f74aec4760e911
James McCarthy <jamesmccarthyxci@gmail.com>
2020-04-09 14:26:28 -0400

    Setup flask-mail and retrieve password route

--- app/app.py
+++ app/app.py
@@ -1,6 +1,9 @@
 from flask import Flask
 from .extensions.sqlalchemy import db
+from .extensions.flask_mail import mail
 from flask_marshmallow import Marshmallow
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_mail import Mail, Message
 
 # Views
 from .views.test import test
@@ -17,8 +20,17 @@ def create_app():
     app.config[
         "SQLALCHEMY_DATABASE_URI"
     ] = "mysql://admin:r0W[qJnK@localhost:3306/flask_api"
+    app.config["JWT_SECRET_KEY"] = "mr$LMk7UTjkc"
+    app.config['MAIL_SERVER']='smtp.mailtrap.io'
+    app.config['MAIL_PORT'] = 2525
+    app.config['MAIL_USERNAME'] = 'd4643680d3cf6b'
+    app.config['MAIL_PASSWORD'] = '5ed2d9bb46c00d'
+    app.config['MAIL_USE_TLS'] = True
+    app.config['MAIL_USE_SSL'] = False
     db.init_app(app)
+    mail.init_app(app)
     ma = Marshmallow(app)
+    jwt = JWTManager(app)
     # Blueprints
     app.register_blueprint(test)
     app.register_blueprint(scripts)
--- app/views/auth.py
+++ app/views/auth.py
@@ -1,6 +1,9 @@
 from flask import Blueprint, jsonify, request
 from ..models.User import User
 from ..extensions.sqlalchemy import db
+from ..extensions.flask_mail import mail
+from flask_jwt_extended import JWTManager, jwt_required, create_access_token
+from flask_mail import Message
 
 auth = Blueprint("auth", __name__)
 
@@ -15,21 +18,45 @@ def register():
         first_name = request.json["first_name"]
         last_name = request.json["last_name"]
         password = request.json["password"]
-        new_user = User(first_name=first_name, last_name=last_name, email=email, password=password)
+        new_user = User(
+            first_name=first_name, last_name=last_name, email=email, password=password
+        )
         db.session.add(new_user)
         db.session.commit()
         return jsonify(message="User creation successful")
-        
+
     except Exception as err:
         return err
 
 
 @auth.route("/login", methods=["POST"])
-def test():
+def login():
     try:
-        # planets = Planet.query.all()
-        # result = planets_schema.dump(planets)
-        # return jsonify(result)
-        return "DEfdsa"
+        email = request.json["email"]
+        password = request.json["password"]
+        user = User.query.filter_by(email=email, password=password).first()
+        if user:
+            access_token = create_access_token(identity=email)
+            return jsonify(message="authenticated", access_token=access_token)
+        else:
+            return jsonify(message="Login failed"), 401
+    except Exception as err:
+        return err
+
+
+@auth.route("/retrieve-password/<string:email>", methods=["GET"])
+def retrieve_password(email: str):
+    try:
+        user = User.query.filter_by(email=email).first()
+        if user:
+            message = Message(
+                "Your password is " + user.password,
+                sender="support@acme.com",
+                recipients=[email],
+            )
+            mail.send(message)
+            return jsonify(message="Password sent to " + email)
+        else:
+            return jsonify(message="No account with that email"), 401
     except Exception as err:
         return err
